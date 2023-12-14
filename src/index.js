dd915022c04c786e412c08ef97f4bd995f833383
jhossepmartinez <jhossepmv@gmail.com>
2023-12-14 17:50:02 -0300

    added cors to acces api from frontend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const routes = require("./routes/posts_routes")
 
 const PORT = 3000
@@ -6,6 +7,7 @@ const app = express()
 
 // Middlewares
 app.use(express.json())
+app.use(cors())
 
 
 // Routes
